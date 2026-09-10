6ae296b98aed975f9d150338dccf26e440a2259c
YoSpringg <josejr0721@gmail.com>
2026-09-09 23:50:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("#thanksButton");
+const message = document.querySelector("#message");
+
+button.addEventListener("click", function() {
+    message.textContent = "Thanks for checking out Yo_Springg!";
+});
